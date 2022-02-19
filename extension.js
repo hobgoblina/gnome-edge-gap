319559a1052b8d324ec1a5dd265959c41bcb150c
necropolina <lina@spectraldiscord.com>
2022-02-18 20:32:14 -0500

    the extinshun

---
+++ extension.js
@@ -0,0 +1,89 @@
+const Main = imports.ui.main;
+const St = imports.gi.St;
+const ExtensionUtils = imports.misc.extensionUtils;
+const Me = ExtensionUtils.getCurrentExtension();
+
+let bottomPanel = null;
+let topPanel = null;
+let leftPanel = null;
+let rightPanel = null;
+let settings = null;
+
+
+function init () {}
+
+function enable() {
+  settings = ExtensionUtils.getSettings();
+  let monitor = Main.layoutManager.primaryMonitor;
+
+  bottomPanel = new St.Bin({
+    reactive: false,
+    can_focus: false,
+    track_hover: false,
+    height: settings.get_int('gap-size'),
+    width: monitor.width,
+  });
+
+  topPanel = new St.Bin({
+    reactive: false,
+    can_focus: false,
+    track_hover: false,
+    height: settings.get_int('gap-size'),
+    width: monitor.width,
+  });
+
+  leftPanel = new St.Bin({
+    reactive: false,
+    can_focus: false,
+    track_hover: false,
+    height: monitor.height,
+    width: settings.get_int('gap-size'),
+  });
+
+  rightPanel = new St.Bin({
+    reactive: false,
+    can_focus: false,
+    track_hover: false,
+    height: monitor.height,
+    width: settings.get_int('gap-size'),
+  });
+
+  bottomPanel.set_position(0, monitor.height - settings.get_int('gap-size'));
+  topPanel.set_position(0, 0);
+  leftPanel.set_position(0, 0);
+  rightPanel.set_position(monitor.width - settings.get_int('gap-size'), 0);
+
+
+  Main.layoutManager.addChrome(bottomPanel, {
+    affectsInputRegion: true,
+    affectsStruts: true,
+  });
+
+  Main.layoutManager.addChrome(topPanel, {
+    affectsInputRegion: true,
+    affectsStruts: true,
+  });
+
+  Main.layoutManager.addChrome(leftPanel, {
+    affectsInputRegion: true,
+    affectsStruts: true,
+  });
+
+  Main.layoutManager.addChrome(rightPanel, {
+    affectsInputRegion: true,
+    affectsStruts: true,
+  });
+  
+  settings.connect('changed::gap-size', function (k, b) {
+    disable();
+    enable();
+  });
+}
+
+function disable() {
+  Main.layoutManager.removeChrome(bottomPanel);
+  Main.layoutManager.removeChrome(topPanel);
+  Main.layoutManager.removeChrome(leftPanel);
+  Main.layoutManager.removeChrome(rightPanel);
+  settings.run_dispose();
+}
---
+++ prefs.js
@@ -0,0 +1,47 @@
+const Gtk = imports.gi.Gtk;
+const GObject = imports.gi.GObject;
+
+const Gettext = imports.gettext.domain('gnome-edge-gap');
+const _ = Gettext.gettext;
+
+const ExtensionUtils = imports.misc.extensionUtils;
+const Me = ExtensionUtils.getCurrentExtension();
+
+let settings;
+
+function init() {
+  settings = ExtensionUtils.getSettings();
+  ExtensionUtils.initTranslations("gnome-edge-gap");
+}
+
+function buildPrefsWidget() {
+  const isGtk4 = Gtk.get_major_version() >= '4';
+
+  let frame = new Gtk.ScrolledWindow(
+      { hscrollbar_policy: Gtk.PolicyType.NEVER });
+  let builder = new Gtk.Builder();
+  builder.set_translation_domain("gnome-edge-gap");
+  settingsPath = isGtk4 ? '/Settings-40.ui' : '/Settings.ui'
+  builder.add_from_file(Me.path + settingsPath);
+
+  let notebook = builder.get_object("settings_notebook");
+  if (isGtk4) {
+      frame.set_child(notebook);
+  } else {
+      frame.add(notebook);
+  }
+
+  let settings_spin = builder.get_object("spin_gap_size");
+  settings_spin.set_value(settings.get_int('gap-size'));
+  settings_spin.connect('value-changed', function (w) {
+      settings.set_int('gap-size', w.get_value());
+  });
+  settings.connect('changed::gap-size', function (k, b) {
+      settings_spin.set_value(settings.get_int(b));
+  });
+
+  if (!isGtk4) {
+      frame.show_all();
+  }
+  return frame;
+}
